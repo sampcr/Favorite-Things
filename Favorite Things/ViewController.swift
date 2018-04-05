660cb1ad9f4e401ba5db093c7ea1d74f55cc31c1
Christopher Robert Samp <sampcr@rose-hulman.edu>
2018-04-05 12:14:07 -0400

    Initial Commit

---
+++ Favorite Things/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Favorite Things
+//
+//  Created by Christopher Samp on 4/5/18.
+//  Copyright © 2018 Christopher Samp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
